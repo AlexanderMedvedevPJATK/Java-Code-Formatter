2a8556cb7a4935658be043078505273de1151d23
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-04-20 19:52:37 +0200

    initial commit

---
+++ src/test/java/com/s28572/tpo07/Tpo07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s28572.tpo07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
